2e0b1c039fda0aae23ef78a8715c97507d7cb3b9
Ashton Sham <ash@Ashtons-MacBook-Pro.local>
2020-05-12 17:59:41 -0400

    208

---
+++ src/card.js
@@ -0,0 +1,15 @@
+import Reacy from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='robot' src=''/>
+            <div>
+                <h2>Jane Doe</h2>
+                <p>jane.doe@gmail.com</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Hello from './Hello';
+import card from './card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(
-    <Hello />,
+    <Hello greeting={'Hello'}/>,
   document.getElementById('root')
 );
 
